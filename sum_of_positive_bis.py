35375ea4baa7af57085afa6321bd0caba07b5306
Julien ESCOFFIER <julien.escoffier@protonmail.com>
2020-04-01 21:43:20 +0200

    Add files via upload

---
+++ sum_of_positive_bis.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# You get an array of numbers, return the sum of all of the positives ones.
+# Example [1,-4,7,12] => 1 + 7 + 12 = 20
+# Note: if there is nothing to sum, the sum is default to 0.
+
+def positive_sum(arr):
+    return sum(x for x in arr if x > 0)
+
+print(positive_sum([-1,2,3,-5]))
+
+
