3fa6391a947d5a610fff011078b3bedb3c4399ab
Julien ESCOFFIER <julien.escoffier@protonmail.com>
2020-04-03 16:30:38 +0200

    Add files via upload

---
+++ 8kyu/return_negative.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# In this simple assignment you are given a number
+#  and have to make it negative. But maybe the number is already negative?
+
+
+def make_negative( number ):
+    if number > 0:
+        return number-number*2
+    elif number <0:
+        return number
+
+
+print(make_negative(42))
